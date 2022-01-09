abc97db47294cef5118beba649eab2ae1cf8e0c6
CHRIS_LEE <chrismlee87@gmail.com>
2022-01-09 00:36:20 -0800

    Initialze [add] nextjs project with tailwind and purge styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,tsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
