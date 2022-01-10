0e921e16989aa8f59d46fb9bbf26478bb367ba27
CHRIS_LEE <chrismlee87@gmail.com>
2022-01-09 22:21:01 -0800

    [update] styling for navbar with search and icon components

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -30,13 +30,13 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 item-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 item-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search" />
         </div>
       </div>
 
-      {/* center */}
+      {/* Center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2 ">
           {/* Send props to HeaderIcon component */}
@@ -47,7 +47,7 @@ function Header() {
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
-      {/* right */}
+      {/* Right */}
     </div>
   )
 }
